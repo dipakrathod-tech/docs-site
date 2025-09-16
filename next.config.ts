41caa29dd2f00f66ac5af17742cb798ce8b4443f
dipak rathod <mr.dipaknrathod@gmail.com>
2025-09-17 01:34:31 +0530

    initial nextra setup

---
+++ next.config.ts
@@ -0,0 +1,14 @@
+import nextra from 'nextra'
+ 
+const withNextra = nextra({
+  
+})
+ 
+export default withNextra({
+ turbopack: {
+   resolveAlias: {
+     // Path to your `mdx-components` file with extension
+     'next-mdx-import-source-file': './src/mdx-components.tsx'
+   }
+ }
+})
